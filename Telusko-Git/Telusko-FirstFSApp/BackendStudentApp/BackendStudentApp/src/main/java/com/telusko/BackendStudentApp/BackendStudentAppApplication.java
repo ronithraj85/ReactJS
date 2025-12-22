279fc2f665dfb4ce77dfa3d62e4ac8add7bc5917
ronith <ronithrajkopperla@gmail.com>
2025-12-22 20:18:02 +0530

    Added swagger implementation

--- Telusko-Git/Telusko-FirstFSApp/BackendStudentApp/BackendStudentApp/src/main/java/com/telusko/BackendStudentApp/BackendStudentAppApplication.java
+++ Telusko-Git/Telusko-FirstFSApp/BackendStudentApp/BackendStudentApp/src/main/java/com/telusko/BackendStudentApp/BackendStudentAppApplication.java
@@ -1,9 +1,19 @@
 package com.telusko.BackendStudentApp;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title="Student Application",version = "1.5",description = "This API is about Student management"
+		),
+		servers = @Server(url = "http://localhost:8080",
+		description = "This is server info where our app is deployed")
+)
 public class BackendStudentAppApplication {
 
 	public static void main(String[] args) {
--- Telusko-Git/Telusko-FirstFSApp/BackendStudentApp/BackendStudentApp/src/main/java/com/telusko/BackendStudentApp/web/StudentController.java
+++ Telusko-Git/Telusko-FirstFSApp/BackendStudentApp/BackendStudentApp/src/main/java/com/telusko/BackendStudentApp/web/StudentController.java
@@ -3,6 +3,9 @@ package com.telusko.BackendStudentApp.web;
 import com.telusko.BackendStudentApp.model.Student;
 import com.telusko.BackendStudentApp.service.IStudentService;
 import java.util.List;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,23 +13,27 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name = "StudentInfiAPIs",description = "APIS to register student and get their details")
 //@CrossOrigin(origins = "http://localhost:5173")
 public class StudentController {
 
     @Autowired
     private IStudentService service;
 
+    @Operation(summary = "GET", description ="API will give all the student details" )
     @GetMapping("/students")
     public List<Student> getAllStudents(){
         return  service.getAllStudents();
     }
 
+    @Operation(summary="POST", description = "Add/Create a new student")
     @PostMapping("/student")
     public Student registerStudent(@RequestBody Student student)
     {
         return service.saveStudent(student);
     }
 
+    @Operation(summary="GET", description = "Gets  a specific student details based on the id")
     @GetMapping("/student/{id}")
     public ResponseEntity<Student> getStudentById(@PathVariable Long id){
         Student student = service.getStudentById(id);
@@ -34,6 +41,7 @@ public class StudentController {
         // return ResponseEntity.ok(student);  Both are same;
     }
 
+    @Operation(summary="PUT", description = "Updates the student details based on the id, returns the updated student details")
     @PutMapping("/updateStudent/{id}")
     public ResponseEntity<Student> getStudentById(@PathVariable Long id, @RequestBody Student student){
         Student s=service.getStudentById(id);
@@ -47,6 +55,7 @@ public class StudentController {
         return ResponseEntity.ok(st);
     }
 
+    @Operation(summary="DELETE ", description = "Deletes the student details")
     @DeleteMapping("/student/{id}")
     public ResponseEntity<Student> deleteStudentById(@PathVariable Long id){
         service.deleteStudent(id);;
