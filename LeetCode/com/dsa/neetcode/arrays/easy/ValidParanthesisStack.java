f0f524f6953d3f2fc176fe9df9b9a3fccfaf4195
Ronith Raj <ronithrajkopperla@gmail.com>
2026-01-25 00:45:29 +0530

    DSA-25-Jan-2026

---
+++ LeetCode/com/dsa/neetcode/arrays/easy/ValidParanthesisStack.java
@@ -0,0 +1,28 @@
+package com.dsa.neetcode.arrays.easy;
+
+import java.util.Stack;
+
+class checkForValidParanthesis{
+    public boolean isValidParanthesis(String s){
+        Stack<Character> stack = new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='(' || ch=='{' || ch=='['){
+                stack.push(ch);
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char top = stack.pop();
+                if((ch==')' && top!='(') || (ch=='}' && top!='{') || (ch==']' && top!='[')){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+}
+
+public class ValidParanthesisStack {
+    public static void main(String[] args) {
+
+    }
+}
